41c92c0756c6294e72e9285df98b6e2182e0b2b7
whati001 <andreas.karner@student.tugraz.at>
2020-10-04 17:08:11 +0200

    AK - demo version

--- src/js/qnsChapters/qnsLaw.js
+++ src/js/qnsChapters/qnsLaw.js
@@ -9,371 +9,39 @@ export default {
   name: 'Rechtskunde',
   qs: [{
     id: 1,
-    q: 'Wett- und Preisfischen in Fließgewässern sind',
+    q: 'Fischen im Fliessgewaesser ist',
     a: {
-      a: 'generell erlaubt.',
-      b: 'bei Kostendeckung erlaubt.',
-      c: 'generell verboten.'
+      a: 'erlaubt.',
+      b: 'bei verwendung der Fische erlaubt',
+      c: 'verboten'
     },
     c: 'c'
-  }
-    , {
-    id: 2,
-    q: 'Welche der folgenden Fangmethoden ist ohne Ausnahmegenehmigung in Fließgewässern erlaubt?',
-    a: {
-      a: 'Sprengstoff',
-      b: 'Trauper',
-      c: 'Angel'
-    },
-    c: 'c'
-  }
-    , {
-    id: 3,
-    q: 'An welchen der folgenden Stellen ist der Fischfang erlaubt?',
-    a: {
-      a: 'Im Stauraum',
-      b: 'In Fischpässen',
-      c: 'In Laichbiotopen'
-    },
-    c: 'a'
-  }
-    , {
-    id: 4,
-    q: 'Was ist zur Erlangung der Fischerkarte in der Steiermark notwendig?',
-    a: {
-      a: 'Tageskarte',
-      b: 'Praktischer Nachweis',
-      c: 'Fischerprüfung'
-    },
-    c: 'c'
-  }
-    , {
-    id: 5,
-    q: 'Unter welcher Voraussetzung dürfen Minderjährige unter 14 Jahren fischen?',
-    a: {
-      a: 'Sie fischen nur in der Äschenregion.',
-      b: 'Sie fischen nur in der Forellenregion.',
-      c: 'Sie befinden sich in Begleitung und unter Aufsicht eines berechtigten Fischers.'
-    },
-    c: 'c'
-  }
-    , {
-    id: 6,
-    q: 'Welche Unterlagen muss man beim Fischfang mitführen?',
-    a: {
-      a: 'Führerschein',
-      b: 'Gültiger Erlaubnisschein und Fischerkarte',
-      c: 'Reisepass'
-    },
-    c: 'b'
-  }
-    , {
-    id: 7,
-    q: 'Welcher der folgenden Köder darf keine Verwendung finden?',
-    a: {
-      a: 'Natürliche Köder',
-      b: 'Künstliche Köder',
-      c: 'Lebende Wirbeltiere'
-    },
-    c: 'c'
-  }
-    , {
-    id: 8,
-    q: 'Unter welchen Bedingungen ist das Wettfischen in stehenden Gewässern erlaubt?',
-    a: {
-      a: 'Wenn eine Verwertung der gefangenen Fische erfolgt.',
-      b: 'Wenn nur vom Ufer aus gefischt wird.',
-      c: 'Wenn das Gewässer eine Wasserfläche von mindestens 0,5 ha besitzt.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 9,
-    q: 'Welche der folgenden Köder dürfen Sie verwenden?.',
-    a: {
-      a: 'Lebende Frösche',
-      b: 'Lebende Rotaugen',
-      c: 'Tote Rotaugen'
-    },
-    c: 'c'
-  }
-    , {
-    id: 10,
-    q: 'Welche der folgenden Köder dürfen Sie verwenden?',
-    a: {
-      a: 'Lebende Wasserspitzmäuse',
-      b: 'Lebende Fische',
-      c: 'Regenwürmer'
-    },
-    c: 'c'
-  }
-    , {
-    id: 11,
-    q: 'Können bereits erteilte Fischerkarten oder Scheine wieder eingezogen werden?',
-    a: {
-      a: 'Ja, wenn der Fischerkarteninhaber gegen fischereiliche Schutzbestimmungen verstoßen hat.',
-      b: 'Nein, selbst wenn der Fischerkarteninhaber gegen fischereiliche Schutzbestimmungen verstoßen hat.',
-      c: 'Nein, auf keinen Fall.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 12,
-    q: 'Fangmethoden wie Schlingen, Betäubungsmittel, Gifte, gentechnisch veränderte Köder, lebende Wirbeltiere und Fischstechen sind',
-    a: {
-      a: 'ausnahmslos verboten.',
-      b: 'an und von der Steiermärkischen Landesregierung zu bewilligende Ausnahmen gebunden.',
-      c: 'ohne Einschränkung erlaubt.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 13,
-    q: 'Welche Maßnahmen dürfen durchgeführt werden?',
-    a: {
-      a: 'Ein Besatz mit gentechnisch veränderten Wassertieren.',
-      b: 'Ein Besatz mit heimischen oder bereits eingebürgerten Wassertieren.',
-      c: 'Ein Besatz mit Wassertieren aller Art.'
-    },
-    c: 'b'
-  }
-    , {
-    id: 14,
-    q: 'Ein Besatz mit Wassertieren‚ die künstlich genetisch verändert worden sind (besonders durch Vervielfältigung des Chromosomensatzes und durch Festlegung auf ein Geschlecht), ist',
-    a: {
-      a: 'ausnahmslos verboten.',
-      b: 'an von der Steiermärkischen Landesregierung zu bewilligende Ausnahmen gebunden.',
-      c: 'ohne Einschränkung erlaubt.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 15,
-    q: 'Wann sind vom Fischereiberechtigten geplante Besatzmaßnahmen der Bezirksverwaltungsbehörde zu melden?',
-    a: {
-      a: 'Eine Woche vor dem geplanten Besatz.',
-      b: 'Zwei Wochen vor dem geplanten Besatz.',
-      c: 'Vier Wochen vor dem geplanten Besatz.'
-    },
-    c: 'b'
-  }
-    , {
-    id: 16,
-    q: 'Gibt es in der Steiermark eine behördlich vorgeschriebene Besatzpflicht?',
-    a: {
-      a: 'Nein, die Behörde kann gar keine Vorgaben machen.',
-      b: 'Ja, eine Besatzpflicht gilt für alle öffentlichen Gewässer.',
-      c: 'Nein. Allerdings kann die Bezirksverwaltungsbehörde die Durchführung eines geplanten, nicht gesetzeskonformen Besatzes aus gewissen Gründen verbieten und anstelle dessen dem Fischereiberechtigten einen bestimmten Fischbesatz vorschreiben.'
-    },
-    c: 'c'
-  }
-    , {
-    id: 17,
-    q: 'Wer führt hauptsächlich die Beaufsichtigung eines Fischwassers durch?',
-    a: {
-      a: 'Ein bestelltes und beeidetes Fischereiaufsichtsorgan.',
-      b: 'Ein Vertreter der Bezirksverwaltungsbehörde.',
-      c: 'Die örtliche Polizeiinspektion.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 18,
-    q: 'Welche Aufgaben hat ein Fischereiaufsichtsorgan laut dem Steiermärkischen Fischereigesetz 2000?',
-    a: {
-      a: 'Den Schutz der Wassertiere sowie die Einhaltung der Bestimmungen des Steiermärkischen Fischereigesetzes 2000 (einschließlich Verordnungen und Bescheide).',
-      b: 'Zählung der Fischfressenden Tiere.',
-      c: 'Verkauf von Erlaubnisscheinen.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 19,
-    q: 'Unter welchen Voraussetzungen dürfen bestehende, private, zum Fischwasser führende Wege befahren werden?',
-    a: {
-      a: 'Zum Zwecke der Ausübung der Fischerei,',
-      b: 'Zur Durchführung von Besatzmaßnahmen oder des Elektrofischfanges.',
-      c: 'Zur Überwachung des Fischwassers.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 20,
-    q: 'Wer ist bei einer geplanten Trockenlegung oder Ausleitung (Abkehr) eines Gewässers mindestens vier Wochen vorher zu verständigen?',
-    a: {
-      a: 'Die zuständige Polizeiinspektion.',
-      b: 'Die Bezirksverwaltungsbehörde.',
-      c: 'Der Fischereiberechtigte.'
-    },
-    c: 'c'
-  }
-    , {
-    id: 21,
-    q: 'Die Durchführung des Elektrofischfanges',
-    a: {
-      a: 'ist ausnahmslos verboten.',
-      b: 'kann von der Bezirksverwaltungsbehörde in Ausnahmefällen bewilligt werden.',
-      c: 'kann vom Fischereiberechtigten jederzeit ohne Genehmigung durchgeführt werden.'
-    },
-    c: 'b'
-  }
-    , {
-    id: 22,
-    q: 'Welche gesetzliche Grundlage regelt das Fischereirecht im Lande Steiermark?',
-    a: {
-      a: 'Das Allgemeine Bürgerliche Gesetzbuch',
-      b: 'Die Österreichische Verfassung',
-      c: 'Das Steiermärkische Fischereigesetz 2000'
-    },
-    c: 'c'
-  }
-    , {
-    id: 23,
-    q: 'Eine Fischerkarte für Jugendliche kann frühestens ausgestellt werden',
-    a: {
-      a: 'an Personen, die das 8 Lebensjahr vollendet haben.',
-      b: 'an Personen, die das 10 Lebensjahr vollendet haben.',
-      c: 'an Personen, die das 14 Lebensjahr vollendet haben.'
-    },
-    c: 'c'
-  }
-    , {
-    id: 24,
-    q: 'Was ist der Fischereikataster?',
-    a: {
-      a: 'Die planliche Darstellung der steirischen Fischwasser auf einer amtlichen Landkarte.',
-      b: 'Eine Auflistung der steirischen Fischerkartenbesitzer.',
-      c: 'Ein Verzeichnis, in dem alle Fischereirechte mit den aktuellen Fischereiberechtigten verzeichnet sind.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 25,
-    q: 'Besatzmaßnahmen sind',
-    a: {
-      a: 'der Bezirksverwaltungsbehörde anzuzeigen.',
-      b: 'von der Gemeinde zu genehmigen.',
-      c: 'vom Amt der Steiermärkischen Landesregierung zu genehmigen.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 26,
-    q: 'Wassertiere im Sinne des Steiermärkischen Fischereigesetzes 2000 sind',
-    a: {
-      a: 'Bisamratte und Graureiher.',
-      b: 'Fische, Krustentiere, Muscheln und Neunaugen.',
-      c: 'Amphibien und Reptilien.'
-    },
-    c: 'b'
   }, {
-    id: 27,
-    q: 'In welcher Weise hat ein Eigentümer, Pächter oder Nutznießer ein Fischwasser zu bewirtschaften?',
+    id: 1,
+    q: 'Fischen im Fliessgewaesser ist',
     a: {
-      a: 'Nach seinem Gutdünken.',
-      b: 'Derart, dass der natürliche Bestand an Wassertieren nach Arten, Zusammensetzung, Altersstufen und Bestandsdichte erhalten bleibt oder wiederhergestellt wird.',
-      c: 'Gar nicht.'
-    },
-    c: 'b'
-  }
-    , {
-    id: 28,
-    q: 'Wer darf natürliche Nahrung von Wassertieren dem Fischwasser entnehmen?',
-    a: {
-      a: 'Nur die zuständige Behörde',
-      b: 'Der Fischereiberechtigte',
-      c: 'Jedermann im Rahmen des Gemeingebrauches'
-    },
-    c: 'b'
-  }
-    , {
-    id: 29,
-    q: 'Wer ist verpflichtet, das Auftreten von Krankheiten an Wassertieren, Fischsterben und Verunreinigungen des Fischwassers anzuzeigen?',
-    a: {
-      a: 'Jeder Staatsbürger',
-      b: 'Nur Exekutivorgane',
-      c: 'Die Fischereiberechtigten, Fischereiaufsichtsorgane und Inhaber von Fischerkarten'
+      a: 'erlaubt.',
+      b: 'bei verwendung der Fische erlaubt',
+      c: 'verboten'
     },
     c: 'c'
-  }
-    , {
-    id: 30,
-    q: 'Zu welchem Zweck ist der Elektrofischfang ohne Einschränkung erlaubt?',
+  }, {
+    id: 1,
+    q: 'Fischen im Fliessgewaesser ist',
     a: {
-      a: 'Aus Gründen der Pflege des Gewässers und des Fischbestandes oder zu wissenschaftlichen Zwecken.',
-      b: 'Bei akuter Gefahr für den Fischbestand.',
-      c: 'Bei einem Mangel an Süßwasserfischen im wirtschaftlichen Verkehr.'
+      a: 'erlaubt.',
+      b: 'bei verwendung der Fische erlaubt',
+      c: 'verboten'
     },
-    c: 'b'
-  }
-    , {
-    id: 31,
-    q: 'Bei welcher Behörde ist um die Genehmigung zur Durchführung des Elektrofischfanges anzusuchen?',
+    c: 'c'
+  }, {
+    id: 1,
+    q: 'Fischen im Fliessgewaesser ist',
     a: {
-      a: 'Bezirksverwaltungsbehörde.',
-      b: 'Steiermärkische Landesregierung.',
-      c: 'Bezirksgericht.'
+      a: 'erlaubt.',
+      b: 'bei verwendung der Fische erlaubt',
+      c: 'verboten'
     },
-    c: 'a'
-  }
-    , {
-    id: 32,
-    q: 'Wer darf ein Fischwasser pachten?',
-    a: {
-      a: 'Jeder, der zumindest drei Jahre lang im Besitz einer gültigen Fischerkarte ist.',
-      b: 'Jeder, der die Fischerprüfung bestanden hat und eine gültige Fischerkarte besitzt.',
-      c: 'Jeder, der den Abschluss eines Pachtvertrages der Bezirksverwaltungsbehörde bezüglich des Eintrags in den Fischereikataster mitgeteilt hat.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 33,
-    q: 'Wem gehören die nach einer Überflutung auf dem Grundstück zurückgebliebenen Fische?',
-    a: {
-      a: 'Dem Grundeigentümer',
-      b: 'Dem Fischereiberechtigen',
-      c: 'Der Tierkörperverwertungsanstalt (TKV)'
-    },
-    c: 'b'
-  }
-    , {
-    id: 34,
-    q: 'Das Betreten umzäunter Ufergrundstücke zur Ausübung der Fischerei ist',
-    a: {
-      a: 'jederzeit erlaubt.',
-      b: 'nur nach vorheriger Genehmigung des Grundbesitzers erlaubt.',
-      c: 'nur nach vorheriger Anmeldung beim Grundbesitzer erlaubt.'
-    },
-    c: 'b'
-  }
-    , {
-    id: 35,
-    q: 'Welchen Geltungsbereich haben Fischergastkarten?',
-    a: {
-      a: 'Gastkarten gelten vier Wochen innerhalb eines Bezirkes.',
-      b: 'Gastkarten gelten einen Tag an einem Gewässer.',
-      c: 'Gastkarten werden für ein Kalenderjahr für alle steirischen Gewässer ausgestellt.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 36,
-    q: 'Welche Vorgaben gibt es in Bezug auf die Schonzeiten?',
-    a: {
-      a: 'Innerhalb der Schonzeit dürfen geschonte Wassertiere weder gezielt befischt, noch entnommen werden.',
-      b: 'Innerhalb der Schonzeit darf gar nicht gefischt werden.',
-      c: 'Innerhalb der Schonzeit darf eine Fischart zwar gezielt befischt, jedoch nicht entnommen werden.'
-    },
-    c: 'a'
-  }
-    , {
-    id: 37,
-    q: 'Wann ist die Ausübung des Fischfanges weidgerecht im Sinne des Steiermärkischen Fischereigesetzes 2000?',
-    a: {
-      a: 'Wenn möglichst viele Fische in möglichst kurzer Zeit gefangen werden.',
-      b: 'Wenn sie den fischereikundlichen Erkenntnissen entspricht und unter Verwendung geeigneter Fanggeräte, Fangvorrichtungen und Fangmittel ausgeübt wird.',
-      c: 'Wenn ein Gewässer flächendeckend abgefischt und dann neu besetzt wird.'
-    },
-    c: 'b'
+    c: 'c'
   }]
 };
